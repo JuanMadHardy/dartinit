6f0c813ceb088910adfa705502a198c07189e910
Juan <kuko810@gmail.com>
2026-05-18 17:48:41 +0200

    create some clases and methods

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,13 +1,10 @@
 // ignore_for_file: unused_import
 
-import 'dart:io';
-
 import 'package:command_runner/command_runner.dart';
-import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
-void main(List<String> arguments) async {
-  var runner = CommandRunner();
-  await runner.run(arguments);
+void main(List<String> arguments) {
+  var commandRunner = CommandRunner()..addCommand(HelpCommand());
+  commandRunner.run(arguments);
 }
