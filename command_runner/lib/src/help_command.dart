14888c88b03729e2dbbfdfb0accafb83b5028a7c
Juan <kuko810@gmail.com>
2026-05-18 17:49:21 +0200

    create some clasess and methods

---
+++ command_runner/lib/src/help_command.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: unused_import
+
+import 'dart:async';
+
+import 'arguments.dart';
+
+class HelpCommand extends Command {
+  HelpCommand() {
+    addFlag(
+      'verbose',
+      abbr: 'v',
+      help: 'When true, this command will pirnt each command and its options.',
+    );
+    addOption(
+      'command',
+      abbr: 'c',
+      help:
+          'When command is passed as an argument, prints only that commands verbose usage.',
+    );
+  }
+
+  @override
+  String get name => 'help';
+
+  @override
+  String get description => 'Prints usage information to the command line.';
+
+  @override
+  String? get help => 'Prints this usage information';
+
+  @override
+  FutureOr<Object?> run(ArgResults args) async {
+    var usage = runner.usage;
+    for (var command in runner.commands) {
+      usage += '\n ${command.usage}';
+    }
+    return usage;
+  }
+}
